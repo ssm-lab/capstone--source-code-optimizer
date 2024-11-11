4fdcfb3d18133a34ea4e654fb4fc20b229c7e0ff
Nivetha Kuruparan <niv.kuru@gmail.com>
2024-11-10 22:35:40 -0500

    Changed ending message

--- tests/input/ineffcient_code_example_1.py
+++ tests/input/ineffcient_code_example_1.py
@@ -22,12 +22,12 @@ def contains_special_numbers(numbers):
 
 def all_lowercase(strings):
     # List comprehension inside `all()` - triggers R1729
-    return all([s.islower() for s in strings])
+    return all(s.islower() for s in strings)
 
 def any_even_numbers(numbers):
     # List comprehension inside `any()` - triggers R1729
-    return any([num % 2 == 0 for num in numbers])
+    return any(num % 2 == 0 for num in numbers)
 
 def all_strings_start_with_a(strings):
     # List comprehension inside `all()` - triggers R1729
-    return all([s.startswith('A') for s in strings])
+    return all(s.startswith('A') for s in strings)
