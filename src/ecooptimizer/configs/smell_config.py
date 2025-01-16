76d9f97525f11efcad10e7c2fc788636cf8fb719
Nivetha Kuruparan <niv.kuru@gmail.com>
2025-01-16 04:23:57 -0500

    Removed smell severity for smell config

--- src/ecooptimizer/configs/smell_config.py
+++ src/ecooptimizer/configs/smell_config.py
@@ -1,5 +1,4 @@
 from ast import AST
-from enum import Enum
 from pathlib import Path
 from typing import Callable
 
@@ -23,60 +22,45 @@ from ..refactorers.member_ignoring_method import MakeStaticRefactorer
 from ..refactorers.long_parameter_list import LongParameterListRefactorer
 
 
-# Just an example of how we can add characteristics to the smells
-class SmellSeverity(Enum):
-    LOW = "low"
-    MEDIUM = "medium"
-    HIGH = "high"
-
-
 # Centralized smells configuration
 SMELL_CONFIG = {
     "use-a-generator": {
         "id": "R1729",
-        "severity": SmellSeverity.MEDIUM,
         "analyzer_method": "pylint",
         "refactorer": UseAGeneratorRefactorer,
     },
     "long-parameter-list": {
         "id": "R0913",
-        "severity": SmellSeverity.MEDIUM,
         "analyzer_method": "pylint",
         "refactorer": LongParameterListRefactorer,
     },
     "no-self-use": {
         "id": "R6301",
-        "severity": SmellSeverity.MEDIUM,
         "analyzer_method": "pylint",
         "refactorer": MakeStaticRefactorer,
     },
     "repeated-calls": {
         "id": "CRC001",
-        "severity": SmellSeverity.MEDIUM,
         "analyzer_method": detect_repeated_calls,
         "refactorer": CacheRepeatedCallsRefactorer,
     },
     "long-lambda-expression": {
         "id": "LLE001",
-        "severity": SmellSeverity.MEDIUM,
         "analyzer_method": detect_long_lambda_expression,
         "refactorer": LongLambdaFunctionRefactorer,
     },
     "long-message-chain": {
         "id": "LMC001",
-        "severity": SmellSeverity.MEDIUM,
         "analyzer_method": detect_long_message_chain,
         "refactorer": LongMessageChainRefactorer,
     },
     "unused_variables_and_attributes": {
         "id": "UVA001",
-        "severity": SmellSeverity.MEDIUM,
         "analyzer_method": detect_unused_variables_and_attributes,
         "refactorer": RemoveUnusedRefactorer,
     },
     "long-element-chain": {
         "id": "LEC001",
-        "severity": SmellSeverity.MEDIUM,
         "analyzer_method": detect_long_element_chain,
         "refactorer": LongElementChainRefactorer,
     },
