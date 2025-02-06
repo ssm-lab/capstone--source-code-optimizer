07a9365052f4ff606afd4ed3a2ee1b998279e7ee
Ayushi Amin <amina23@mcmaster.ca>
2025-02-05 22:14:59 -0500

    Updated test cases for #343

--- tests/input/project_multi_file_lec/src/processor.py
+++ tests/input/project_multi_file_lec/src/processor.py
@@ -5,11 +5,12 @@ def process_data(data):
     Process some data and call the long_element_chain method from Utility.
     """
     util = Utility()
-    result = util.long_element_chain()
-    value1 = result["level1"]["level2"]["level3"]["level4"]["level5"]["level6"]["level7"]
-    value2 = util.get_value(result)
-    print(f"Extracted Value1: {value1}")
-    print(f"Extracted Value2: {value2}")
+    my_call = util.long_chain["level1"]["level2"]["level3"]["level4"]["level5"]["level6"]["level7"]
+    lastVal = util.get_last_value()
+    fourthLevel = util.get_4th_level_value()   
+    print(f"My call here: {my_call}")
+    print(f"Extracted Value1: {lastVal}")
+    print(f"Extracted Value2: {fourthLevel}")
     return data.upper()
 
 
